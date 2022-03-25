ec2f04250063e9d97b667d073b6ebec175894dde
Rohit Barichello <rohitbarichello@tamu.edu>
2022-03-25 14:35:16 -0500

    Lab 1: Sending over 4bit stim file

---
+++ fulladder_run1/INCA_libs/irun.lnx8664.15.20.nc/.timestamp.ts
@@ -0,0 +1,4 @@
+1648228307 /home/ugrads/r/rohitbarichello/ECEN454/fulladder_run1/ihnl/cds0/netlist
+1139589200 /opt/coe/ncsu/ncsu-cdk-1.6.0.beta/lib/NCSU_Digital_Parts/xor2/functional/verilog.v
+1139589197 /opt/coe/ncsu/ncsu-cdk-1.6.0.beta/lib/NCSU_Digital_Parts/nand2/functional/verilog.v
+1648228307 /home/ugrads/r/rohitbarichello/ECEN454/fulladder_run1/testfixture.template
---
+++ fulladder_run1/INCA_libs/irun.lnx8664.15.20.nc/files.ts
@@ -0,0 +1,5 @@
+1612436527 /home/ugrads/r/rohitbarichello/cds.lib
+1648228307 /home/ugrads/r/rohitbarichello/ECEN454/fulladder_run1/testfixture.template
+1139589197 /opt/coe/ncsu/ncsu-cdk-1.6.0.beta/lib/NCSU_Digital_Parts/nand2/functional/verilog.v
+1139589200 /opt/coe/ncsu/ncsu-cdk-1.6.0.beta/lib/NCSU_Digital_Parts/xor2/functional/verilog.v
+1648228307 /home/ugrads/r/rohitbarichello/ECEN454/fulladder_run1/ihnl/cds0/netlist
