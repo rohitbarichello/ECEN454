ea1de8ad654aac0433eb2f0ff516869381b698c1
Rohit Barichello <rohitbarichello@tamu.edu>
2022-03-25 14:58:40 -0500

    Lab1: sending over 8bit

---
+++ fourBitAdder_run1/INCA_libs/irun.lnx8664.15.20.nc/.timestamp.ts
@@ -0,0 +1,5 @@
+1648236835 /home/ugrads/r/rohitbarichello/ECEN454/fourBitAdder_run1/ihnl/cds0/netlist
+1648236835 /home/ugrads/r/rohitbarichello/ECEN454/fourBitAdder_run1/testfixture.template
+1139589200 /opt/coe/ncsu/ncsu-cdk-1.6.0.beta/lib/NCSU_Digital_Parts/xor2/functional/verilog.v
+1648236835 /home/ugrads/r/rohitbarichello/ECEN454/fourBitAdder_run1/ihnl/cds1/netlist
+1139589197 /opt/coe/ncsu/ncsu-cdk-1.6.0.beta/lib/NCSU_Digital_Parts/nand2/functional/verilog.v
